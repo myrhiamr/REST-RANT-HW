0beeef91ad4f3d9a2aad02aa35c4e20972f6967b
Myrhiam Rodriguez <myrhiamrodriguez@Myrhiams-MacBook-Air.local>
2024-03-11 21:16:18 -0700

    method delet added

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,10 +8,18 @@ function show(data) {
         <h1>{data.place.name}</h1>
         <img src={data.place.pic} alt={data.place.name} />
         <p>
-          Located in {data.place.city}, {data.place.state} and serving {" "}
+          Located in {data.place.city}, {data.place.state} and serving{" "}
           {data.place.cuisines}
         </p>
       </main>
+      <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        Edit
+      </a>
+      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <button type="submit" className="btn btn-danger">
+          Delete
+        </button>
+      </form>
     </Def>
   );
 }
