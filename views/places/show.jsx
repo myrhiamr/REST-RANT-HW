d5407edc6625715f5a7bf7d4ef7e7f83a0176567
Myrhiam Rodriguez <myrhiam74@gmail.com>
2024-03-27 20:28:10 -0700

    part 11

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,6 +8,27 @@ function Show(data) {
     </h3>
   )
 
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+          let stars = ''
+          for (let i = 0; i < averageRating; i++) {
+            stars += 'â­ï¸'
+          }
+    rating = (
+      <h3>
+      { stars } stars
+      </h3>
+    )    
+  }
+  
   if (data.place.comments.length) {
     comments = data.place.comments.map(c => {
       return (
@@ -29,6 +50,9 @@ function Show(data) {
       <main>
         <div className="row">
           <div className="col-sm-6">
+            <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+            { rating }
             <img src={data.place.images} alt={data.place.name} /> 
             <h3>
               Located in {data.place.city}, {data.place.state}
@@ -45,6 +69,9 @@ function Show(data) {
             <h2>Comments</h2>
             { comments } 
           </div>
+
+
+
           <div className="col-sm-6">
       <form action="/comments" method="POST" class="form">    
       
@@ -65,11 +92,27 @@ function Show(data) {
     <label for="rant" class="form-check-label">Rant</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-     
+</form>     
     </div>
         </div>
+
+        <h1>Edit Place</h1>
+<form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+    <div className="row">
+        <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input className="form-control" 
+              id="founded" 
+              name="founded" 
+              value={data.place.founded} 
+            />
+        </div>
+    </div>
+    <input className="btn btn-primary" type="submit" value="Update Place" />
+</form>
+
+
+
 
         <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit </a>
         <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
